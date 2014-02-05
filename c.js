e80422f23847dd1b6872106c4dd3eb72f49640a8
sp3pd <sp3pd@mail.umkc.edu>
2014-02-04 20:36:24 -0600

    commit

---
+++ c.js
@@ -0,0 +1,42 @@
+$('#control').ready(function() {
+    initialize(41.850033, -87.6500523);
+    $('#submit').click(function(){
+        
+        displayRoute();      
+    });
+});
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+
+function initialize(lat,lan) {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+    zoom: 7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: new google.maps.LatLng(lat, lan)
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+  var control = document.getElementById('control');
+  control.style.display = 'block';
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+}
+
+function displayRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+    
